e1b1132b21dbbce3be4d8c5aec7b2e1ac5858f48
Addison <addison@yunio.com>
2019-05-06 19:48:00 +0800

    model fileds

--- models/talk.js
+++ models/talk.js
@@ -3,6 +3,9 @@ var Schema = mongoose.Schema;
 
 var talkSchema = new Schema({
   auther: { type: Schema.Types.ObjectId, ref: 'user' },
+  text: { type: String },
+  nickname: { type: String },
+  avatar: { type: String },
   status: { type: Number, default: 1 }, // 0:删除
   like: { type: Array, default: [] },
   commit: { type: Array, default: [] }, // [ {replay_id: uuid, user_id: ba65..., text: '回复', commit: [ {replay: uuid, user_id: c656..., text: '回复ba65', commit: [] } ]}, {...} ]
